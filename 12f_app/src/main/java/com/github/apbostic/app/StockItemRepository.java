65736575324f6aef82a35ca6365dda554fcb1300
Pat Bostic <pat.bostic@sas.com>
2017-03-29 15:29:31 -0400

    initial commit

---
+++ 12f_app/src/main/java/com/github/apbostic/app/StockItemRepository.java
@@ -0,0 +1,10 @@
+package com.github.apbostic.app;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.github.apbostic.app.domain.model.ItemType;
+import com.github.apbostic.app.domain.model.StockItem;
+
+public interface StockItemRepository extends CrudRepository<StockItem, Long> {
+    List<StockItem> findByType(ItemType type);
+}
